38c1c91d0e8617b9e3a060f16fb7c9c9e95034f0
Parker Collins <127002162+parkercollins@users.noreply.github.com>
2023-07-27 12:01:51 -0600

    update rules.mk to also source features/casemodes.c

--- keyboards/lily58/keymaps/parkercollins/rules.mk
+++ keyboards/lily58/keymaps/parkercollins/rules.mk
@@ -15,6 +15,7 @@ SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        features/casemodes.c
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
