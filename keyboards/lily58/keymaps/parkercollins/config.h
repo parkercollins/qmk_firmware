bce83d22c8e308e31830323ac9c4abde0d48593b
Parker Collins <127002162+parkercollins@users.noreply.github.com>
2023-11-16 13:43:11 -0700

    update tapping term for tap dance

--- keyboards/lily58/keymaps/parkercollins/config.h
+++ keyboards/lily58/keymaps/parkercollins/config.h
@@ -26,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define QUICK_TAP_TERM 100
+#define TAPPING_TERM 175
 
 #undef RGBLED_NUM
 #define RGBLIGHT_EFFECT_BREATHING
@@ -46,7 +46,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
-//Caps word 
+//Caps word
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
 // Underglow
