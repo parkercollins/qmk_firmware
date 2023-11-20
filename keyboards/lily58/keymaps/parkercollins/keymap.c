ecab82518b73d40dfaaaa2fa7e1a3c9a0eefe7ed
Parker Collins <127002162+parkercollins@users.noreply.github.com>
2023-11-20 07:08:14 -0700

    rename keypad to nav

--- keyboards/lily58/keymaps/parkercollins/keymap.c
+++ keyboards/lily58/keymaps/parkercollins/keymap.c
@@ -10,7 +10,7 @@ enum custom_keycodes {
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
-  _KEYPAD,
+  _Nav,
   _ADJUST,
 };
 // Tap Dance declarations
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |ESCAPE|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |KEBABCASE|
+ * |ESC,GRV|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |KEBABCASE|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   TD(TD_ESC_GRAVE), KC_1,              KC_2,      KC_3,   KC_4,   KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KEBABCASE,
   KC_TAB,           KC_Q,              KC_W,      KC_E,   KC_R,   KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_HYPR,          LT(_KEYPAD, KC_A), KC_S,      KC_D,   KC_F,   KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_HYPR,          LT(_Nav, KC_A), KC_S,      KC_D,   KC_F,   KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,          KC_Z,              KC_X,      KC_C,   KC_V,   KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                                             KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, CAMELCASE, KC_BSPC, SNAKECASE
 ),
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
-/* Keypad
+/* Nav
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
-[_KEYPAD] = LAYOUT(
+[_Nav] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _KEYPAD, _ADJUST);
+  return update_tri_layer_state(state, _LOWER, _Nav, _ADJUST);
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
